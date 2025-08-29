a1e5f4eaf16cd7d7d477e605e93b9be512f55176
ANUSHKA SINGH <anushkathakur201@gmail.com>
2025-08-30 00:51:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -211,5 +211,5 @@ def summarize():
 
 
 if __name__ == "__main__":
-    print("🚀 Starting Flask Document Summarizer...")
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=os.environ.get("PORT", 5000))
+
